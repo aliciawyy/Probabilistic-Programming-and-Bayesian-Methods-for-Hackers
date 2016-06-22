0e38412638c883fc1d9f7d5e6f0e01c1da37f84d
Alice Wang <alice@forevercapital.com>
2016-06-22 13:38:11 +0200

    update reading notes in chapter 1 and 2

---
+++ src/import_helper.py
@@ -0,0 +1,22 @@
+"""
+Import helper
+
+Can be used in each chapter
+
+import sys
+sys.path.append("../src")
+from import_helper import *
+%matplotlib inline
+
+===
+import json
+import matplotlib
+s = json.load(open("../styles/bmh_matplotlibrc.json"))
+matplotlib.rcParams.update(s)
+
+"""
+
+from IPython.core.pylabtools import figsize
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.stats as stats
